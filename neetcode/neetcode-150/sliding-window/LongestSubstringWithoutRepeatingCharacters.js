0629b18150c0962b5cd4d81e4238fa7b3444b595
anrivlev <ivlev@itgeo.ru>
2026-09-07 16:08:53 +0300

    LongestSubstringWithoutRepeatingCharacters.js второе решение

--- neetcode/neetcode-150/sliding-window/LongestSubstringWithoutRepeatingCharacters.js
+++ neetcode/neetcode-150/sliding-window/LongestSubstringWithoutRepeatingCharacters.js
@@ -1,5 +1,6 @@
 class Solution {
   /**
+   * Решение с Set
    * @param {string} s
    * @return {number}
    */
@@ -20,6 +21,33 @@ class Solution {
         } while (leftChar !== char);
       }
       visited.add(char);
+      const length = right - left + 1;
+      if (length > maxLength) maxLength = length;
+      right++;
+    }
+    return maxLength;
+  }
+}
+
+class Solution {
+  /**
+   * Решение с Map (оптимальнее по итерациям с left)
+   * @param {string} s
+   * @return {number}
+   */
+  lengthOfLongestSubstring(s) {
+    if (s.length === 0) return 0;
+    let maxLength = 0;
+    let left = 0;
+    let right = 0;
+    const visited = new Map();
+    while (right < s.length) {
+      let char = s[right];
+      const previousIndex = visited.get(char);
+      if (previousIndex !== undefined && previousIndex >= left) {
+        left = previousIndex + 1;
+      }
+      visited.set(char, right);
       const length = right - left + 1;
       if (length > maxLength) maxLength = length;
       right++;
