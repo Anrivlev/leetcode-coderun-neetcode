1fc4ffa042f27cf215fc6a98b1d74423f31a7f3d
anrivlev <ivlev@itgeo.ru>
2026-09-16 17:30:10 +0300

    тяжелые задачки

--- neetcode/neetcode-150/sliding-window/MinimumWindowSubstring.js
+++ neetcode/neetcode-150/sliding-window/MinimumWindowSubstring.js
@@ -11,8 +11,21 @@ class Solution {
     }
     let left = 0;
     let right = 0;
+    let readyCounter = frequencies.size;
+    let minWindowSize = Infinity;
+    let indices = [-1, -1];
     while (right < s.length) {
+      s[right];
+      const frequency = (frequencies.get(s[right]) ?? 0) + 1;
+      if (frequency === 0) readyCounter--;
+      while (readyCounter === 0) {
         
+        left++;
+      }
+      right++;
+      if (readyCounter === 0 && right - left + 1 < minWindowSize)
+        minWindowSize = right - left + 1;
     }
+    return minWindowSize === Infinity ? '' : s.slice(indices[0], indices[1] + 1);
   }
 }
