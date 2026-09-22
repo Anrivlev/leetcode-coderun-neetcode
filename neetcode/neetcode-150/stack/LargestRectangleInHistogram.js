b3e5084d1c6de4c1d5bf4b20e8945e899d8fe03b
anrivlev <ivlev@itgeo.ru>
2026-09-22 16:12:48 +0300

    BinarySearch.js оставляю hard задачки на выходные, когда высплюсь у будет голова посвежее. В рабочие часы вообще не решаются

---
+++ neetcode/neetcode-150/stack/LargestRectangleInHistogram.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    largestRectangleArea(heights) {
+        let maxArea = 0;
+        for (const height of heights) {
+            
+        }
+        return maxArea;
+    }
+}
